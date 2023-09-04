948a454f0601ccd8d19c76b601b808f2bd8c78c2
davidbryan20 <davidbryan20@icloud.com>
2023-09-04 11:38:04 -0300

    Feito lista de produtos e atualizado o react icon

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,13 +1,13 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 
 export default function EditarProdutos() {
-
+    //Recuperando o ID do produto selecionado!
     const {id} = useParams();
 
   return (
     <div>
         <h1>EDITAR - PRODUTOS</h1>
-        <p>PRODUTO SELECIONADO - </p>
+        <p>PRODUTO SELECIONADO - {id}</p>
     </div>
   )
 }
